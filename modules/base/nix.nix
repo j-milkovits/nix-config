d3ad898e4b0412f4a359f16504083264db19b60a
Jonas Milkovits <j.milkovits.t@posteo.net>
2026-06-28 11:12:40 +0200

    feat(nix): add nix-community and nixpkgs-unfree binary caches

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -6,7 +6,20 @@
 
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    # substituters = ["https://cache.nixos.org"];
+
+    # appended to the default substituters (cache.nixos.org), not replacing them
+    extra-substituters = [
+      # community packages (home-manager, nix4nvchad, NUR, ...)
+      "https://nix-community.cachix.org"
+      # unfree packages that Hydra refuses to build (bambu-studio, ...)
+      "https://nixpkgs-unfree.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      # from https://app.cachix.org/cache/nix-community
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # from https://app.cachix.org/cache/nixpkgs-unfree
+      "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+    ];
   };
 
   nixpkgs.config.allowUnfree = lib.mkDefault true;
